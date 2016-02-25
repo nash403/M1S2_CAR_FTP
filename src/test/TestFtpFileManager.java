e1de223cc5f17aec7aed8aee4abfcfe28da3737a
LucasDelvallet <lucas.delvallet@hotmail.fr>
2016-02-25 21:35:24 +0100

    - Ajout de 5 test sur les chemins - Ajout d'une majuscule manquante sur un nom de classe.

---
+++ src/test/TestFtpFileManager.java
@@ -0,0 +1,91 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import ftpServer.FtpFileManager;
+import ftpServer.FtpResponse;
+
+public class TestFtpFileManager {
+
+	@Test
+	public void testGetWD() {
+		try {
+			FtpFileManager FFM = new FtpFileManager("", "");
+			assertEquals("ftproot", FFM.getWD());	
+		} catch (IOException e) {
+			//Not supposed to happen
+		}
+	}
+	
+	@Test
+	public void testGetFullWD(){
+		try {
+			FtpFileManager FFM = new FtpFileManager("", "");
+			assertEquals(new java.io.File("").getAbsolutePath() + "\\ftproot", FFM.getFullWD());	
+		} catch (IOException e) {
+			//Not supposed to happen
+		}
+	}
+	
+	@Test
+	public void testChangeWDIfFolderExist(){
+		try {
+			FtpFileManager FFM = new FtpFileManager("", "");
+			String testFolder = "testDir";
+			File dir = new File(new java.io.File("").getAbsolutePath() + "\\ftproot\\" + testFolder);
+			dir.mkdir();
+			
+			assertEquals(FtpResponse.cwd_ok,FFM.changeWD(testFolder));
+			dir.delete();
+	
+		} catch (IOException e) {
+			//Not supposed to happen
+		}
+	}
+	
+	@Test
+	public void testChangeWDIfNavigationWork(){
+		try {
+			FtpFileManager FFM = new FtpFileManager("", "");
+			String testFolder = "testDir";
+			File dir = new File(new java.io.File("").getAbsolutePath() + "\\ftproot\\" + testFolder);
+			dir.mkdir();
+			FFM.changeWD(testFolder);
+			
+			assertEquals(testFolder, FFM.getWD());
+			dir.delete();
+	
+		} catch (IOException e) {
+			//Not supposed to happen
+		}
+	}
+	
+	@Test
+	public void testChangeWDIfFolderDontExist(){
+		try {
+			FtpFileManager FFM = new FtpFileManager("", "");
+			String testFolder = "UnexistentDir";
+
+			assertEquals(FtpResponse.no_such_file+"ftproot/"+testFolder,FFM.changeWD(testFolder));
+
+	
+		} catch (IOException e) {
+			//Not supposed to happen
+		}
+	}
+
+}
